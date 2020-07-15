524f53e3320df4936e914629353de11d00c843c1
qiqingfu <9116895@qq.com>
2020-07-15 16:31:25 +0800

    examples add modules demo

---
+++ examples/modules/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello, Vuex Module
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+  },
+  mounted () {
+    console.log(this.$store)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/modules/store/index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by qiqf on 2020/5/17
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const moduleA = {
+  state: () => ({
+    moduleName: 'A'
+  }),
+  mutations: { },
+  actions: { },
+  getters: { }
+}
+
+const moduleB = {
+  state: () => ({
+    moduleName: 'B'
+  }),
+  mutations: { },
+  actions: { }
+}
+
+const store = new Vuex.Store({
+  modules: {
+    a: moduleA,
+    b: moduleB
+  }
+})
+
+export default store
